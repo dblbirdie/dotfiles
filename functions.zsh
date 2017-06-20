62e9f8bba1638ad2c21f9993f629a59145cfdd91
dblbirdie <dblbirdie@users.noreply.github.com>
2017-06-20 11:26:14 +0200

    Update dotfiles on a regular basis, add bundles, and include functions.zsh

---
+++ functions.zsh
@@ -0,0 +1,42 @@
+#
+# Functions
+#
+
+# Open repository on GitHub etc.
+# Source: https://dev.to/shayde/open-the-github-project-page-of-a-repo-from-terminal/
+alias gitopen=GitOpen
+
+function GitOpen()
+{
+    if [ ! -d .git ] ; 
+        then echo "ERROR: This isn't a git directory" && return false; 
+    fi
+    git_url=`git config --get remote.origin.url`
+    if [[ $git_url != https://* ]] ;
+        then echo "ERROR: Remote origin is invalid" && return false;
+    fi
+    url=${git_url%.git}
+    open $url
+}
+
+
+# Open info website for a given http status code
+alias statuscode=HttpStatusCode
+
+function HttpStatusCode()
+{
+    base_url=https://httpstatuses.com/
+    error_code=$1
+    url="$base_url$1"
+
+    if [[ "$error_code" != <-> || "${#error_code}" -ne 3 ]]
+        then echo "ERROR: The http status code must be a three digit number." && return false;
+    fi
+
+    website_status=$(curl -sLI -w "%{http_code}" "$url" -o /dev/null)
+    #if [ $? -ne 0 ]
+    if [[ $? -ne 0 || "$website_status" -eq "404" ]]
+        then echo "ERROR: In all probability, the http status code does not exist." && return false;
+    fi
+    open $url;
+}
--- main.zsh
+++ main.zsh
@@ -1,15 +1,36 @@
-# zsh config
 #
+# This is the main dotfile. Source this file in your ~/.zshrc
+#
+
+# Get current directory and use it as base directory
 BASEDIR=$(dirname "$0")
 
-# Load Antigen and "Oh My ZSH!"
-source /usr/local/share/antigen/antigen.zsh
-antigen bundle robbyrussell/oh-my-zsh lib/
+# Update dotfiles if there are any changes in the remote repository
+(cd $BASEDIR;
+if [ `git rev-list --count origin/master..HEAD` -gt 0 ]; then
+  git pull
+fi
+)
+
+# Load Antigen
+if [ ! -f $HOME/antigen/antigen.zsh ]; then
+  git clone git://github.com/zsh-users/antigen.git $HOME/antigen
+fi
+source $HOME/antigen/antigen.zsh
+
+#antigen use oh-my-zsh	# init antigen with oh-my-zsh
 
 # Antigen Bundles
+antigen bundle robbyrussell/oh-my-zsh lib/
 antigen bundle git
 antigen bundle rupa/z
 antigen bundle sublime
+#antigen bundle aws
+#antigen bundle brew
+#antigen bundle osx
+#antigen bundle ssh-agent
+#antigen bundle urltools
+#antigen bundle vagrant
 
 # Antigen Theme
 antigen theme agnoster
@@ -20,10 +41,12 @@ autoload zmv
 
 # Custom adjustments
 source $BASEDIR/aliases.zsh
+source $BASEDIR/functions.zsh
 source $BASEDIR/custom-completion.zsh
 
 # Setup autocomplete for kubectl
 command -v kubectl >/dev/null && source <(kubectl completion zsh)
+command -v helm >/dev/null && source <(helm completion zsh)
 
 # Other stuff
 [[ -n "$SSH_CLIENT" ]] || export DEFAULT_USER="mhuber"
