40fa7700e371357788bfd1d3894e60f8642bfe55
dblbirdie <dblbirdie@users.noreply.github.com>
2018-07-26 17:27:49 +0200

    Remove aliases that make standard commands verbose

--- aliases.zsh
+++ aliases.zsh
@@ -11,10 +11,6 @@ alias finds='grep -rnw . -e $1'
 # ls options: A = include hidden (but not . or ..), F = put `/` after folders, h = byte unit suffixes
 alias ll='ls -AlFh'
 
-# Make some commands verbose
-alias rm='rm -v'
-#alias cp='cp -v'
-
 # Show last Git commits as single lines
 alias lc='git log --oneline | head -10'
 
