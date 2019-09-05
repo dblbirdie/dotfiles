bad7a4e199eacc15c887aba8aed356a49e59b2fc
dblbirdie <dblbirdie@users.noreply.github.com>
2019-09-05 14:41:39 +0200

    Add alias to generate a random 32-digit password

--- aliases.zsh
+++ aliases.zsh
@@ -25,3 +25,4 @@ alias copystats='rsync -a --progress --stats --human-readable'
 alias httpstatuscode='curl -s http://www.ietf.org/assignments/http-status-codes/http-status-codes.txt | grep ' $1 ' | head -1'
 alias beautifyjson='python -m json.tool'
 alias trim="sed '/^[[:space:]]*$/d;s/^[[:space:]]*//;s/[[:space:]]*$//'"
+alias genpass32='openssl rand -base64 32'
