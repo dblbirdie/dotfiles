eb88612cbf3065054c3cc19bfb7450cc67bc1736
dblbirdie <dblbirdie@users.noreply.github.com>
2022-03-19 16:50:57 +0100

    Update functions

--- functions.zsh
+++ functions.zsh
@@ -2,24 +2,6 @@
 # Functions
 #
 
-# Open repository on GitHub etc.
-# Source: https://dev.to/shayde/open-the-github-project-page-of-a-repo-from-terminal/
-alias gitopen=GitOpen
-
-function GitOpen()
-{
-    if [ ! -d .git ] ; 
-        then echo "ERROR: This isn't a git directory" && return false; 
-    fi
-    git_url=`git config --get remote.origin.url`
-    if [[ $git_url != https://* ]] ;
-        then echo "ERROR: Remote origin is invalid" && return false;
-    fi
-    url=${git_url%.git}
-    open $url
-}
-
-
 # Open info website for a given http status code
 alias statuscode=HttpStatusCode
 
@@ -41,30 +23,16 @@ function HttpStatusCode()
     open $url;
 }
 
-# Print markdown files text optimized
-alias markout=MarkDownPrettyPrint
-
-function MarkDownPrettyPrint()
-{
-    md="$1"
-
-    if [ $# -eq 0 ]
-    then
-        md="README.md"
-    fi
-
-    pandoc $md | lynx -stdin -dump
-}
-
-# Create directory and change right into it
-alias md=MakeDirectory
-
-function MakeDirectory()
-{
-    mkdir -p "$1" && cd "$1"
-}
-
 # Copy command line and stdout to the clipboard
 function cpcmd {
     echo "$ $@\n$(zsh -ic $@)" | tee >(pbcopy)
- }
+}
+
+function backup()
+{
+    source_file="${1}"
+    target_file="${1}.bak"
+    cp -v "$source_file" "$target_file" \
+        && trash "$target_file" \
+        && echo "Moved $target_file to trash bin."
+}
