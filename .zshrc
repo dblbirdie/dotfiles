2318e46193f28bfd1b0982d49323f3fa514b46ab
dblbirdie <dblbirdie@users.noreply.github.com>
2022-03-19 16:52:11 +0100

    Improve autosuggestion

--- .zshrc
+++ .zshrc
@@ -12,9 +12,6 @@ antigen use oh-my-zsh
 
 antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-autosuggestions
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=66"
-ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-ZSH_AUTOSUGGEST_USE_ASYNC=true
 antigen bundle git
 antigen bundle rupa/z
 antigen bundle sublime
--- autocomplete.zsh
+++ autocomplete.zsh
@@ -1,1 +1,19 @@
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=66"
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_USE_ASYNC=true
+
+#zle_bracketed_paste=()
+# This speeds up pasting w/ autosuggest
+# https://github.com/zsh-users/zsh-autosuggestions/issues/238
+pasteinit() {
+  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+  zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+}
+
+pastefinish() {
+  zle -N self-insert $OLD_SELF_INSERT
+}
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
